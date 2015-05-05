611f0ec0d088426461671f39cd29665e4a021ead
Ben Cornelis & Alec Arme <student@epicodus.com>
2015-05-05 14:50:54 -0700

    refactor class Patient to include doctor_id argument.

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,16 +1,18 @@
 class Patient
-  attr_reader :name, :birthdate, :id
+  attr_reader :name, :birthdate, :id, :doctor_id
 
   def initialize(attributes)
     @name = attributes.fetch(:name)
     @birthdate = attributes.fetch(:birthdate)
     @id = attributes.fetch(:id)
+    @doctor_id = attributes.fetch(:doctor_id)
   end
 
   def ==(other_patient)
     same_name = @name == other_patient.name
-    same_birthdate = @birthdate== other_patient.birthdate
-    same_name && same_birthdate
+    same_birthdate = @birthdate == other_patient.birthdate
+    same_doctor_id = @doctor_id == other_patient.doctor_id
+    same_name && same_birthdate && same_doctor_id
   end
 
   def self.all
@@ -20,7 +22,8 @@ class Patient
       name = result.fetch("name")
       id = result.fetch("id").to_i
       birthdate = result.fetch("birthdate")
-      patients.push(Patient.new({:name => name, :id => id, :birthdate => birthdate}))
+      doctor_id = result["doctor_id"]
+      patients.push(Patient.new({:name => name, :id => id, :birthdate => birthdate, :doctor_id => doctor_id}))
     end
     patients
   end
